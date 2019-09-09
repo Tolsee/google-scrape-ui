55a59fe9ccbfd100ed8496007585a48d2495c05a
Tolsee <tolsee3@gmail.com>
2019-09-09 22:13:00 +0545

    Add tests for authClient

--- src/utils/__tests__/authClient.js
+++ src/utils/__tests__/authClient.js
@@ -1,6 +1,6 @@
-import { login, singup, logout } from '../authClient';
+import { login, singup, logout, getUser } from '../authClient';
 
-const url = path => `${process.env.REACT_APP_API_URL}/${path}`
+const url = path => `${process.env.REACT_APP_API_ENDPOINT}/${path}`
 
 describe('authClient', () => {
   describe('login', () => {
@@ -72,6 +72,65 @@ describe('authClient', () => {
       expect(window.fetch.mock.calls[0][0]).toEqual(url('signup'));
       expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
       expect(window.localStorage.setItem).toHaveBeenCalledWith('__google_scrape_token__', 'TEST_TOKEN');
+    });
+  });
+
+  describe('getUser', () => {
+    beforeEach(() => {
+      jest.spyOn(Storage.prototype, 'getItem');
+    });
+
+    afterEach(() => {
+      localStorage.getItem.mockRestore();
+    });
+
+    it('should get user when authorization token is present', async () => {
+      const user = { email: 'test@gmail.com' };
+      window.localStorage.setItem('__google_scrape_token__', 'TEST_TOKEN');
+      window.fetch.mockResolvedValueOnce({
+        json: () => Promise.resolve(user),
+        status: 200
+      });
+      const body = await getUser();
+      expect(window.localStorage.getItem).toHaveBeenCalledWith('__google_scrape_token__');
+      expect(window.fetch).toHaveBeenCalledTimes(1);
+      expect(window.fetch).toHaveBeenCalledWith(url('me'), {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer TEST_TOKEN',
+          'content-type': 'application/json',
+        },
+      });
+      expect(body).toEqual(user);
+      window.localStorage.removeItem('__google_scrape_token__');
+    });
+
+    it('should return null when when authorization token is present', async () => {
+      const body = await getUser();
+      expect(body).toEqual(null);
+    });
+
+    it('should get user when authorization token is present', async () => {
+      jest.spyOn(Storage.prototype, 'removeItem');
+
+      window.localStorage.setItem('__google_scrape_token__', 'TEST_TOKEN');
+      window.fetch.mockResolvedValueOnce({
+        json: () => Promise.resolve(),
+        status: 401
+      });
+      const body = await getUser();
+      expect(window.fetch).toHaveBeenCalledTimes(1);
+      expect(window.fetch).toHaveBeenCalledWith(url('me'), {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer TEST_TOKEN',
+          'content-type': 'application/json',
+        },
+      });
+      expect(window.localStorage.removeItem).toHaveBeenCalledWith('__google_scrape_token__');
+      expect(body).toEqual(null);
+      window.localStorage.removeItem('__google_scrape_token__');
+      localStorage.removeItem.mockRestore();
     });
   });
 });
