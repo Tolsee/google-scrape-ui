f87f9d94964450ebc99b5ff9e9ac186bcf37da31
Tolsee <tolsee3@gmail.com>
2019-09-10 01:36:00 +0545

    Employ AuthProvider in application

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { ThemeProvider } from 'styled-components';
-
 import { BrowserRouter } from 'react-router-dom';
+import AppProviders from 'context';
 
 import App from 'App';
 import registerServiceWorker from 'registerServiceWorker';
@@ -14,13 +14,13 @@ import { GlobalStyle, theme } from 'styled';
 // eslint-disable-next-line
 import 'antd/dist/antd.min.css';
 
-export const AppWithProvider = () => (
+const AppWithProvider = () => (
   <BrowserRouter>
     <ThemeProvider theme={theme}>
-      <>
+      <AppProviders>
         <App />
         <GlobalStyle />
-      </>
+      </AppProviders>
     </ThemeProvider>
   </BrowserRouter>
 );
