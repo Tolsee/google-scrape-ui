b23399a5fe8dac8ca09291b6e000449909deb76b
Tolsee <tolsee3@gmail.com>
2019-09-10 01:35:06 +0545

    Basic test for App component

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import { mount } from 'enzyme';
+import AppProviders from 'context';
 
 import App from '../App';
 
+jest.mock('components/common/LoginForm');
+
 describe('App component', () => {
   it('should render app component', () => {
-    const component = mount(<App />);
-
-    expect(component).toMatchSnapshot();
+    mount(
+      <BrowserRouter>
+        <ThemeProvider theme={{}}>
+          <AppProviders>
+            <App />
+          </AppProviders>
+        </ThemeProvider>
+      </BrowserRouter>
+    );
   });
 });
