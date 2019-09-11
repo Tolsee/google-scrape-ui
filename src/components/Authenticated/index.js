8da6e32142904db9a0c371416a332d3d1835e975
Tolsee <tolsee3@gmail.com>
2019-09-11 08:59:16 +0545

    Authenticated component

--- src/components/Authenticated/index.js
+++ src/components/Authenticated/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
+import { KeywordsProvider } from 'context/keywordsContext';
+
+import Header from 'components/common/Header';
+import KeywordUpload from 'components/Authenticated/KeywordUpload';
+import Keywords from 'components/Authenticated/Keywords';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+`;
+
+function Layout({children}) {
+  return (
+    <Container>
+      <Header />
+      {children}
+    </Container>
+  )
+}
 export default function Authenticated() {
-  return <div>Authenticated</div>
+  return (
+    <KeywordsProvider>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Keywords} />
+          <Route path="/upload" component={KeywordUpload} />
+          <Redirect to="/" />
+        </Switch>
+      </Layout>
+    </KeywordsProvider>
+  )
 }
